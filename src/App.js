0f7a30865b6641e634f10a1742bed3eabb44ee61
Albert Sanchez <albert.san.iza@gmail.com>
2018-06-28 18:23:59 -0700

    clean src folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 class App extends Component {
   render() {
     return (
@@ -14,8 +13,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
