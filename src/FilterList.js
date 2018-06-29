98795935d97783b96dabdac4459608c6c84c7fab
Albert Sanchez <albert.san.iza@gmail.com>
2018-06-29 16:08:46 -0700

    add fade in to FilterList

--- src/FilterList.js
+++ src/FilterList.js
@@ -4,11 +4,11 @@ class FilterList extends Component {
     render() {
         if(this.props.data && this.props.data.length > 0) {
             return(
-                <div className="row pt-4">
+                <div className="row pt-4 fade-in">
                 <div className="col-md-12">
                 <h5>
                 Filters Applied:
-                {this.props.data.map((y, z) => <span className="badge badge-secondary" key={z}>{y.title} - {y.value}</span>)}
+                {this.props.data.map((y, z) => <span className="badge badge-secondary fade-in" key={z}>{y.title} - {y.value}</span>)}
                 </h5>
                 </div>
                 </div>
