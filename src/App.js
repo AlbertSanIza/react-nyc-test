f8ea6ed75ab6e5e9cbb92fdfe24f2c71365c08d8
Albert Sanchez <albert.san.iza@gmail.com>
2018-06-28 22:45:38 -0700

    add props

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import './App.css'
 
 class Filter extends Component {
-    constructor() {
-        super()
+    constructor(props) {
+        super(props)
         this.state = {
             value: "ALL"
         }
