2f272fa8c483cf75ce0d7fdcae5bc28ea60b4f81
Albert Sanchez <albert.san.iza@gmail.com>
2018-06-28 20:57:07 -0700

    get mock data

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            serverData: []
+        }
+        this.handleClick = this.handleClick.bind(this)
+    }
+    handleClick() {
+        axios.get('mock-data.json').then(response => {
+            var responseData = response.data.data
+            for (var i = 0; i < responseData.length; i++) {
+                responseData[i] = [responseData[i][8], responseData[i][9], responseData[i][10]]
+            }
+        })
+    }
     render() {
         return (
-            <div></div>
+            <div>
+            <button className='button' onClick={this.handleClick}>Click Me</button>
+            </div>
         )
     }
 }
