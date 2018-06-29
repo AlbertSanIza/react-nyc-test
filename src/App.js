cbe7ee437864519778b4b3a269a05c1eca5116ae
Albert Sanchez <albert.san.iza@gmail.com>
2018-06-29 01:17:18 -0700

    check if function exists

--- src/App.js
+++ src/App.js
@@ -34,7 +34,9 @@ class Filter extends Component {
     }
     handleFilterChange(evt) {
         this.setState({value: evt.target.value})
-        this.props.onFilterChange({title: this.props.title, value: evt.target.value})
+        if (typeof this.props.onFilterChange === "function") {
+            this.props.onFilterChange({title: this.props.title, value: evt.target.value})
+        }
     }
     render() {
         return(
