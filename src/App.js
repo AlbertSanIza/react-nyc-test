3fe61822e35c9e2a2c16b432ebcbe9b665aacdf0
Albert Sanchez <albert.san.iza@gmail.com>
2018-06-29 12:27:12 -0700

    moved filter to different file

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Bar } from 'react-chartjs-2'
+import Filter from './Filter.js'
 import './App.css'
-
-class Filter extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: "ALL"
-        }
-        this.handleFilterChange = this.handleFilterChange.bind(this)
-    }
-    handleFilterChange(evt) {
-        this.setState({value: evt.target.value})
-        if (typeof this.props.onFilterChange === "function") {
-            this.props.onFilterChange({title: this.props.options[0], value: evt.target.value})
-        }
-    }
-    render() {
-        return(
-            <div className="col-sm-4 form-group">
-            <label>{this.props.options[0] ? this.props.options[0] : "Title"}:</label>
-            <select className="form-control" value={this.state.value} onChange={this.handleFilterChange} disabled={this.props.options ? (this.props.options.length < 1) : true}>
-            {this.props.options ? (this.props.options.map((y, z) => z > 0 ? <option key={z} value={y}>{y}</option> : "")) : false}
-            </select>
-            </div>
-        )
-    }
-}
 
 class FiltersApplied extends Component {
     render() {
