f1f710422155da152f26bac33680215b31e4aa3b
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-04 22:51:54 -0700

    data getter component

---
+++ src/Data.js
@@ -0,0 +1,45 @@
+import { Component } from 'react'
+import axios from 'axios'
+
+class Data extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            server: []
+        }
+        this.getServerData()
+    }
+    getServerData = () => {
+        axios.get('https://data.cityofnewyork.us/api/views/25th-nujf/rows.json').then(response => {
+            var columnName = []
+            response.data.meta.view.columns.forEach(z => {
+                columnName.push(z.name.toUpperCase())
+            })
+            var responseData = response.data.data
+            responseData.unshift(columnName)
+            for(var i = 0; i < responseData.length; i++) {
+                responseData[i] = [
+                    responseData[i][8], responseData[i][9], responseData[i][10], responseData[i][11].toUpperCase()
+                ]
+                switch (responseData[i][2]) {
+                    case "WHITE NON HISP":
+                    responseData[i][2] = "WHITE NON HISPANIC"
+                    break
+                    case "BLACK NON HISP":
+                    responseData[i][2] = "BLACK NON HISPANIC"
+                    break
+                    case "ASIAN AND PACI":
+                    responseData[i][2] = "ASIAN AND PACIFIC ISLANDER"
+                    break
+                    default: break
+                }
+            }
+            this.props.onDataChange(responseData)
+        })
+    }
+    render() {
+        return null
+    }
+}
+
+export default Data
