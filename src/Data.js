facb7433f22f92c3779fcbf2d82e96428adee9b8
Albert Sanchez <albert.san.iza@gmail.com>
2018-07-06 21:20:00 -0700

    updated function and immproved performance

--- src/Data.js
+++ src/Data.js
@@ -41,31 +41,13 @@ class Data extends Component {
     }
     getFilterOptions = data => {
         var filters = []
-        var arrayData = data.slice()
-        for(var i = 0; i < arrayData.length; i++) {
-            var smallArray = []
-            for (var j = 0; j < this.props.filter.length; j++) {
-                if(data[0].indexOf(this.props.filter[j]) !== -1) {
-                    smallArray.push(arrayData[i][data[0].indexOf(this.props.filter[j])].toUpperCase())
-                }
-            }
-            arrayData[i] = smallArray
-        }
-        if(arrayData[0].length > 0) {
-            for(var a = 0; a < arrayData[0].length; a++) {
-                filters.push([arrayData[0][a], "ALL"])
-            }
-            for(var b = 1; b < data.length; b++) {
-                for(var c = 0; c < arrayData[b].length; c++) {
-                    var found = false
-                    for(var d = 0; d < filters[c].length; d++) {
-                        if(filters[c][d] === arrayData[b][c]) {
-                            found = true
-                            break
-                        }
-                    }
-                    if(!found) {
-                        filters[c].push(arrayData[b][c])
+        for(var i = 0; i < this.props.filter.length; i++) {
+            var index = data[0].indexOf(this.props.filter[i])
+            if(index > -1) {
+                filters.push([this.props.filter[i], "ALL"])
+                for(var j = 0; j < data.length; j++) {
+                    if(!filters[i].includes(data[j][index])) {
+                        filters[i].push(data[j][index])
                     }
                 }
             }
